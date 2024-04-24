2501b77013b01768f82bd5412d3506779bba9706
LIDERWAR <106931671+LIDERWAR@users.noreply.github.com>
2024-04-24 16:36:39 +0300

    Add files via upload

---
+++ lesson79/main.js
@@ -0,0 +1,25 @@
+const firstNumber = 5
+const secondNumber = 5
+
+let result = null
+
+if (firstNumber == secondNumber) {
+    const result = firstNumber + secondNumber
+    console.log(result)
+} else {
+    const result = secondNumber -firstNumber
+    console.log(result)
+}
+
+const alex ='admin'
+alex == 'admin' ? console.log('Alex is admin') : console.log('Alex is not admin')
+// ======================================
+const userAge = 38;
+
+if (userAge >= 18) {
+    console.log('Вы совершеннолетний')
+} else {
+    console.log('Вы малолетка!!!')
+}
+
+userAge >= 18 ? console.log('Вы совершеннолетний') : console.log('Вы малолетка!!!'); 
